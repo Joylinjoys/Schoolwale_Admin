8c751c6122aa6c2f37b08e871cc5addcda958607
joylin <joylin.jd@gmail.com>
2023-07-01 13:43:58 +0530

    Comiited by joylinds

--- lib/screens/Result.dart
+++ lib/screens/Result.dart
@@ -1,7 +1,53 @@
 import 'package:flutter/material.dart';
 
-class ResultPage extends StatelessWidget {
+class ResultPage extends StatefulWidget {
   const ResultPage({Key? key}) : super(key: key);
+
+  @override
+  _ResultPageState createState() => _ResultPageState();
+}
+
+class _ResultPageState extends State<ResultPage> {
+  final _formKey = GlobalKey<FormState>();
+  final _examNameController = TextEditingController();
+  final _regNoController = TextEditingController();
+  final _subjectNameController = TextEditingController();
+  final _totalMarksController = TextEditingController();
+  final _passingMarksController = TextEditingController();
+  final _marksObtainedController = TextEditingController();
+
+  @override
+  void dispose() {
+    _examNameController.dispose();
+    _regNoController.dispose();
+    _subjectNameController.dispose();
+    _totalMarksController.dispose();
+    _passingMarksController.dispose();
+    _marksObtainedController.dispose();
+    super.dispose();
+  }
+
+  void _submitForm() {
+    if (_formKey.currentState?.validate() ?? false) {
+      // Form is valid, perform form submission
+      final examNameValue = _examNameController.text;
+      final regNoValue = _regNoController.text;
+      final subjectNameValue = _subjectNameController.text;
+      final totalMarksValue = _totalMarksController.text;
+      final passingMarksValue = _passingMarksController.text;
+      final marksObtainedValue = _marksObtainedController.text;
+
+      // Example: Print form values
+      print('Exam Name: $examNameValue');
+      print('Registration No: $regNoValue');
+      print('Subject Name: $subjectNameValue');
+      print('Total Marks: $totalMarksValue');
+      print('Passing Marks: $passingMarksValue');
+      print('Marks Obtained: $marksObtainedValue');
+
+      // TODO: Handle form submission and further actions
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -18,95 +64,173 @@ class ResultPage extends StatelessWidget {
         backgroundColor: Colors.deepPurple.shade400,
       ),
       body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(60.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-             mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Center(
-                child: Text(
-                  'Enter the Examination Details',
-                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-                ),
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(60.0),
+            child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Text(
+                      'Enter the Examination Details',
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  SizedBox(height: 16),
+                  Center(
+                    child: Column(
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Enter Examination Name:',
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 8),
+                            Container(
+                              width: 200,
+                              child: TextFormField(
+                                controller: _examNameController,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  contentPadding:
+                                  EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Enter Student Reg. No:',
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 8),
+                            Container(
+                              width: 200,
+                              child: TextFormField(
+                                controller: _regNoController,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  contentPadding:
+                                  EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Enter Subject Name:',
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 8),
+                            Container(
+                              width: 200,
+                              child: TextFormField(
+                                controller: _subjectNameController,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  contentPadding:
+                                  EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Total Marks:',
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 8),
+                            Container(
+                              width: 100,
+                              child: TextFormField(
+                                controller: _totalMarksController,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  contentPadding:
+                                  EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Text(
+                              'Passing Marks:',
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 8),
+                            Container(
+                              width: 100,
+                              child: TextFormField(
+                                controller: _passingMarksController,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  contentPadding:
+                                  EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'Marks Obtained:',
+                              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(width: 8),
+                            Container(
+                              width: 100,
+                              child: TextFormField(
+                                controller: _marksObtainedController,
+                                decoration: InputDecoration(
+                                  border: OutlineInputBorder(),
+                                  contentPadding:
+                                  EdgeInsets.symmetric(vertical: 8, horizontal: 4),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: _submitForm,
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.deepPurple, // Set button color to purple
+                              minimumSize: Size(200, 50), // Increase button size
+                            ),
+                            child: Text(
+                              'Submit',
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-              SizedBox(height: 16),
-              Text(
-                'Enter Examination Name:',
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(height: 8),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Enter Student Reg. No:',
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(height: 8),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Enter Subject Name:',
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(height: 8),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Total Marks:',
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(height: 8),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Passing Marks:',
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(height: 8),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16),
-              Text(
-                'Marks Obtained:',
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(height: 8),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 16),
-              ElevatedButton(
-                onPressed: () {
-                  // Handle button press
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: Colors.deepPurple, // Set button color to purple
-                ),
-                child: Text('Submit'),
-              ),
-            ],
+            ),
           ),
         ),
       ),
--- lib/screens/Resultfirst.dart
+++ lib/screens/Resultfirst.dart
@@ -22,108 +22,122 @@ class Resultfirst extends StatelessWidget {
         child: Center(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Center(
-                  child: Text(
-                    'Enter Student Results',
-                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            child: Container(
+              alignment: Alignment.topCenter,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Center(
+                    child: Text(
+                      'Enter Student Results',
+                      style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
                   ),
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Select Class:',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(width: 8),
-                    Container(
-                      width: 500, // Set the desired width
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
+                  SizedBox(height: 16),
+                  LayoutBuilder(
+                    builder: (BuildContext context, BoxConstraints constraints) {
+                      final maxWidth = constraints.maxWidth;
+                      final textFieldWidth = maxWidth > 500 ? 500.0 : maxWidth * 0.8;
+
+                      return Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Select Class:',
+                                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(width: 8),
+                              Container(
+                                width: textFieldWidth,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Select Section:',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(width: 8),
-                    Container(
-                      width: 500, // Set the desired width
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
+                          SizedBox(height: 16),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Select Section:',
+                                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(width: 8),
+                              Container(
+                                width: textFieldWidth,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      'Select Rollno',
-                      style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    SizedBox(width: 8),
-                    Container(
-                      width: 500, // Set the desired width
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.circular(10.0),
+                          SizedBox(height: 16),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                'Select Rollno',
+                                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                              SizedBox(width: 8),
+                              Container(
+                                width: textFieldWidth,
+                                child: TextField(
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
                           ),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 16),
-                Center( // Center the "Enter" button
-                  child: GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => const ResultPage()),
+                          SizedBox(height: 16),
+                          Center(
+                            child: GestureDetector(
+                              onTap: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => const ResultPage()),
+                                );
+                              },
+                              child: Container(
+                                width: 200,
+                                decoration: BoxDecoration(
+                                  color: Colors.deepPurple.shade400,
+                                  borderRadius: BorderRadius.circular(8),
+                                ),
+                                padding: EdgeInsets.symmetric(vertical: 16),
+                                child: Center(
+                                  child: Text(
+                                    'Enter',
+                                    style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
                       );
                     },
-                    child: Container(
-                      width: 200,
-                      decoration: BoxDecoration(
-                        color: Colors.deepPurple.shade400,
-                        borderRadius: BorderRadius.circular(8),
-                      ),
-                      padding: EdgeInsets.symmetric(vertical: 16),
-                      child: Center(
-                        child: Text(
-                          'Enter',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
