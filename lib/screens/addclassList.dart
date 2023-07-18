7a21f666543d30d3ebcd7e61f54784d97ec0e8d4
HARSHINI <harshpoojary178@gmail.com>
2023-07-18 20:07:55 +0530

    commit

--- lib/screens/addclassList.dart
+++ lib/screens/addclassList.dart
@@ -6,151 +6,157 @@ import 'Editclass.dart';
 class AddPageList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Classes/Section List',
-          style: TextStyle(
-            fontSize: 29,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-        backgroundColor: Colors.deepPurple.shade400,
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: FirebaseFirestore.instance.collection('ClassSections').snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          }
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('ClassSections').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Error: ${snapshot.error}');
+        }
 
-          if (!snapshot.hasData) {
-            return Center(child: CircularProgressIndicator());
-          }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
 
-          final classList = snapshot.data!.docs.map((doc) => doc.data() as Map<String, dynamic>).toList();
+        List<Map<String, dynamic>> classList = snapshot.data!.docs.map((doc) {
+          return doc.data() as Map<String, dynamic>;
+        }).toList();
 
-          return Center(
-            child: DataTable(
-              columnSpacing: 10.0,
-              dividerThickness: 3.0,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.black,
-                  width: 1.0,
-                ),
+        return Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            title: Text(
+              'Classes/Section List',
+              style: TextStyle(
+                fontSize: 29,
+                fontWeight: FontWeight.bold,
               ),
-              columns: const [
-                DataColumn(
-                  label: Text(
-                    'Class',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
+            ),
+            backgroundColor: Colors.deepPurple.shade400,
+          ),
+          body: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(40.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    'Classes/Section List',
+                    style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                   ),
-                ),
-                DataColumn(
-                  label: Text(
-                    'Sections',
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-                DataColumn(
-                  label: SizedBox(),
-                ),
-              ],
-              rows: classList.asMap().entries.map((entry) {
-                final index = entry.key;
-                final classInfo = entry.value;
-
-                String className = classInfo['class'].toString();
-                List<String> sections = (classInfo['sections'] as List<dynamic>).cast<String>();
-
-                return DataRow(
-                  cells: [
-                    DataCell(
-                      Container(
-                        width: 100,
-                        child: Text(
-                          className,
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                    DataCell(
-                      Container(
-                        width: 100,
-                        child: Text(
-                          sections.join(', '),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                    DataCell(
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.end,
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (context) => EditClassSectionPage(classInfo: classInfo),
-                                ),
-                              );
-                            },
-                            icon: Icon(Icons.edit),
-                            color: Colors.deepPurple,
+                  SizedBox(height: 20),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: DataTable(
+                      columnSpacing: 100.0,
+                      columns: [
+                        DataColumn(
+                          label: Text(
+                            'Class',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                          IconButton(
-                            onPressed: () async {
-                              showDialog(
-                                context: context,
-                                builder: (BuildContext context) {
-                                  return AlertDialog(
-                                    title: Text('Delete Class'),
-                                    content: Text('Are you sure you want to delete this class?'),
-                                    actions: [
-                                      TextButton(
-                                        onPressed: () {
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: Text('CANCEL'),
-                                      ),
-                                      TextButton(
-                                        onPressed: () async {
-                                          await FirebaseFirestore.instance
-                                              .collection('ClassSections')
-                                              .doc(classInfo['class'])
-                                              .delete();
-                                          Navigator.of(context).pop();
-                                        },
-                                        child: Text('DELETE'),
-                                      ),
-                                    ],
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Sections',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Edit',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                        DataColumn(
+                          label: Text(
+                            'Delete',
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                      rows: classList.map((classInfo) {
+                        return DataRow(
+                          cells: [
+                            DataCell(
+                              Text(classInfo['class'] ?? ''),
+                            ),
+                            DataCell(
+                              Text(classInfo['sections'] != null
+                                  ? (classInfo['sections'] as List<dynamic>).join(', ')
+                                  : ''),
+                            ),
+                            DataCell(
+                              IconButton(
+                                onPressed: () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => EditClassSectionPage(classInfo: classInfo),
+                                    ),
                                   );
                                 },
-                              );
-                            },
-                            icon: Icon(Icons.delete),
-                            color: Colors.red,
-                          ),
-                        ],
-                      ),
+                                icon: Icon(Icons.edit),
+                                color: Colors.deepPurple,
+                              ),
+                            ),
+                            DataCell(
+                              IconButton(
+                                onPressed: () {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) {
+                                      return AlertDialog(
+                                        title: Text('Delete Class'),
+                                        content: Text('Are you sure you want to delete this class?'),
+                                        actions: [
+                                          TextButton(
+                                            onPressed: () {
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: Text('CANCEL'),
+                                          ),
+                                          TextButton(
+                                            onPressed: () async {
+                                              await FirebaseFirestore.instance
+                                                  .collection('ClassSections')
+                                                  .doc(classInfo['class'])
+                                                  .delete();
+                                              Navigator.of(context).pop();
+                                            },
+                                            child: Text('DELETE'),
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  );
+                                },
+                                icon: Icon(Icons.delete),
+                                color: Colors.red,
+                              ),
+                            ),
+                          ],
+                        );
+                      }).toList(),
                     ),
-                  ],
-                );
-              }).toList(),
+                  ),
+                ],
+              ),
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
-
