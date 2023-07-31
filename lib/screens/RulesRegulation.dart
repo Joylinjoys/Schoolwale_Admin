602dcd34435cb0d01906497ecfa7a05342b7e20b
joylin <joylin.jd@gmail.com>
2023-07-31 15:03:52 +0530

    Comiited by joylinds

--- lib/screens/RulesRegulation.dart
+++ lib/screens/RulesRegulation.dart
@@ -1,25 +1,24 @@
+
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-class RulesRegulation extends StatefulWidget {
-  @override
-  _RulesRegulationState createState() => _RulesRegulationState();
-}
+class RulesRegulation extends StatelessWidget {
+  final TextEditingController _rulesController = TextEditingController();
 
-class _RulesRegulationState extends State<RulesRegulation> {
-  final TextEditingController _titleController = TextEditingController();
-  final TextEditingController _descriptionController = TextEditingController();
-  bool _isEditing = false;
-  String _currentTitle = '';
-  String _currentDescription = '';
+  void _submitRules() async {
+    String rulesText = _rulesController.text;
 
-  @override
-  void initState() {
-    super.initState();
-    _currentTitle = 'Rules and Regulation';
-    _currentDescription =
-    'All students are expected to greet their school teachers when they meet them whether they actually teach them or not.\n• No books (other than text books or library books), magazines Cds, Pen Drive etc should be brought to school.\n• If they are brought, they will be confiscated.\n• Students are expected to respect school property.\n• No student should damage any school furniture,\n write or draw anything on the walls irregular or in any way damage things belonging to others.\n• Any school property damaged even by accident should be reported at once to the class teacher or to the Principal.\n• A student must always come to school in uniform, even during the Practical and special classes.\n• Chewing chocolates and gum in the school premises is strictly forbidden.\n• No students shall indulge in any of the following practices:\n   - spitting in or near the school building.\n   - disfiguring or damaging any school property.\n   - smoking any form of gambling.\n   - use of drugs or intoxicants except on prescription by a regular medical practitioner.';
-    _titleController.text = _currentTitle;
-    _descriptionController.text = _currentDescription;
+    if (rulesText.isNotEmpty) {
+      try {
+        await FirebaseFirestore.instance.collection('Rules').add({
+          'text': rulesText,
+        });
+        _rulesController.clear();
+        print('Rules submitted and saved to Firebase!');
+      } catch (e) {
+        print('Error saving to Firebase: $e');
+      }
+    }
   }
 
   @override
@@ -36,88 +35,37 @@ class _RulesRegulationState extends State<RulesRegulation> {
         ),
         backgroundColor: Colors.deepPurple.shade400,
       ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            // mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Container(
-                padding: const EdgeInsets.all(16.0),
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.grey),
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextField(
-                      controller: _titleController,
-                      readOnly: !_isEditing,
-                      decoration: InputDecoration(
-                        hintText: 'Title',
-                      ),
-                    ),
-                    SizedBox(height: 16.0),
-                    TextField(
-                      controller: _descriptionController,
-                      readOnly: !_isEditing,
-                      maxLines: null,
-                      decoration: InputDecoration(
-                        hintText: 'Description',
-                      ),
-                    ),
-                  ],
-                ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            TextField(
+              controller: _rulesController,
+              maxLines: null,
+              decoration: InputDecoration(
+                labelText: 'Enter Rules and Regulations',
+                border: OutlineInputBorder(),
               ),
-              SizedBox(height: 30.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        if (_isEditing) {
-                          _currentTitle = _titleController.text;
-                          _currentDescription = _descriptionController.text;
-                        }
-                        _isEditing = !_isEditing;
-                      });
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.deepPurple,
-                      textStyle: TextStyle(fontSize: 20),
-                    ),
-                    child: Text(
-                      _isEditing ? 'Done' : 'Edit',
-                    ),
-                  ),
-                  SizedBox(width: 10.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        _currentTitle = _titleController.text;
-                        _currentDescription = _descriptionController.text;
-                        // Perform update action here with _currentTitle and _currentDescription
-                        print('Title updated: $_currentTitle');
-                        print('Description updated: $_currentDescription');
-                      });
-                    },
-                    style: ElevatedButton.styleFrom(
-                      primary: Colors.deepPurple,
-                      textStyle: TextStyle(fontSize: 20),
-                    ),
-                    child: Text(
-                      'Update',
-                    ),
-                  ),
-                ],
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: _submitRules,
+              style: ElevatedButton.styleFrom(
+                primary: Colors.deepPurple,
+                textStyle: TextStyle(fontSize: 18),
               ),
-            ],
-          ),
+              child: Text('Submit'),
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
+void main() {
+  runApp(MaterialApp(
+    home: RulesRegulation(),
+  ));
+}
