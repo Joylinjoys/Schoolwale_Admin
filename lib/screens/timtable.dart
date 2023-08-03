7571228ed7278bc0ea2c1cded7441e38f4f27c91
veekshitha10 <veekshithabangera10@gmail.com>
2023-08-03 14:26:05 +0530

    commit

--- lib/screens/timtable.dart
+++ lib/screens/timtable.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:web_dashboard_app_tut/screens/addtimetabel.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+// import 'package:image_network/image_network.dart';
+// import 'package:cached_network_image/cached_network_image.dart';
 class TimetableScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -19,6 +20,7 @@ class TimetableScreen extends StatelessWidget {
       ),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisSize: MainAxisSize.min,
         children: [
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 8.0),
@@ -51,41 +53,44 @@ class TimetableScreen extends StatelessWidget {
             ),
           ),
           SizedBox(height: 10.0),
-          Expanded(
-            child: StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance.collection('Timetable').snapshots(),
-              builder: (context, snapshot) {
-                if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                }
-
-                if (!snapshot.hasData) {
-                  return Center(child: Text('No Data Available'));
-                }
-
-                // Extract the documents from the snapshot
-                final documents = snapshot.data!.docs;
-
-                return ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  padding: EdgeInsets.symmetric(horizontal: 10.0),
-                  itemCount: documents.length,
-                  itemBuilder: (context, index) {
-                    // Extract the document data
-                    final data = documents[index].data() as Map<String, dynamic>;
-                    final className = documents[index].id;
-                    final section = data['section'];
-                    final imageUrl = data[className.substring(2)]['imageUrl'];
-
-                    return TimetableCard(
-                      image: imageUrl,
-                      text: '$className',
-                      onDelete: () => _deleteTimetable(className),
-                      onAddImage: () => _updateTimetableImage(className),
-                    );
-                  },
-                );
-              },
+          SizedBox(
+            height: 300.0,
+            child: Expanded(
+              child: StreamBuilder<QuerySnapshot>(
+                stream: FirebaseFirestore.instance.collection('Timetable').snapshots(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasError) {
+                    return Center(child: Text('Error: ${snapshot.error}'));
+                  }
+          
+                  if (!snapshot.hasData) {
+                    return Center(child: Text('No Data Available'));
+                  }
+          
+                  // Extract the documents from the snapshot
+                  final documents = snapshot.data!.docs;
+          
+                  return ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    padding: EdgeInsets.symmetric(horizontal: 10.0),
+                    itemCount: documents.length,
+                    itemBuilder: (context, index) {
+                      // Extract the document data
+                      final data = documents[index].data() as Map<String, dynamic>;
+                      final className = documents[index].id;
+                      final section = data['section'];
+                      final imageUrl = data[className.substring(2)]['imageUrl'];
+                  
+                      return TimetableCard(
+                        image: imageUrl,
+                        text: '$className',
+                        onDelete: () => _deleteTimetable(className),
+                        onAddImage: () => _updateTimetableImage(className),
+                      );
+                    },
+                  );
+                },
+              ),
             ),
           ),
         ],
@@ -122,7 +127,7 @@ class TimetableCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 320,
-      height: 170,
+      height: 200,
       child: Card(
         child: Column(
           children: [
@@ -131,68 +136,71 @@ class TimetableCard extends StatelessWidget {
              // Only show the image if it's not null
               Image.network(
                 image!,
+              //  imageCache: CachedNetworkImageProvider(image),
                 height: 200.0,
                 width: double.infinity,
                 fit: BoxFit.cover,
               ),
             Padding(
               padding: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    text,
-                    style: TextStyle(
-                      fontSize: 12.0,
-                      fontWeight: FontWeight.bold,
+              child: Center(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                    "   " +text,
+                      //textAlign: TextAlign.center,
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
-                  ),
-                  SizedBox(height: 1.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      ElevatedButton(
-                        onPressed: () {
-                          showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: Text('Delete Timetable'),
-                                content: Text('Are you sure you want to delete?'),
-                                actions: [
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context); // Close the dialog
-                                      onDelete(); // Call the delete function
-                                    },
-                                    child: Text('Yes'),
-                                  ),
-                                  TextButton(
-                                    onPressed: () {
-                                      Navigator.pop(context); // Close the dialog
-                                    },
-                                    child: Text('No'),
-                                  ),
-                                ],
+                    SizedBox(height: 1.0),
+                   
+                      
+                          ElevatedButton(
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    title: Text('Delete Timetable'),
+                                    content: Text('Are you sure you want to delete?'),
+                                    actions: [
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context); // Close the dialog
+                                          onDelete(); // Call the delete function
+                                        },
+                                        child: Text('Yes'),
+                                      ),
+                                      TextButton(
+                                        onPressed: () {
+                                          Navigator.pop(context); // Close the dialog
+                                        },
+                                        child: Text('No'),
+                                      ),
+                                    ],
+                                  );
+                                },
                               );
                             },
-                          );
-                        },
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
-                        ),
-                        child: Icon(Icons.delete),
-                      ),
-                      ElevatedButton(
-                        onPressed: onAddImage,
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
-                        ),
-                        child: Icon(Icons.add_a_photo),
-                      ),
-                    ],
-                  ),
-                ],
+                            style: ButtonStyle(
+                              backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
+                            ),
+                            child: Icon(Icons.delete),
+                          ),
+                        
+                        // ElevatedButton(
+                        //   onPressed: onAddImage,
+                        //   style: ButtonStyle(
+                        //     backgroundColor: MaterialStateProperty.all(Colors.deepPurple),
+                        //   ),
+                        //   child: Icon(Icons.add_a_photo),
+                        // ),
+                     
+                  ],
+                ),
               ),
             ),
           ],
